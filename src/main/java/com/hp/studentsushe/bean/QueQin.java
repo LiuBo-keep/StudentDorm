08cee1e38cf762622fda4483b50d10e6cc7c6095
liu <1712624418@qq.com>
2020-01-29 17:39:05 +0800

    new

--- src/main/java/com/hp/studentsushe/bean/QueQin.java
+++ src/main/java/com/hp/studentsushe/bean/QueQin.java
@@ -1,11 +1,15 @@
 package com.hp.studentsushe.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
 @Component
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class QueQin implements Serializable {
     private String id;
